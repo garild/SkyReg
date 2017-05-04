d320d352c9f1b2214859803b8bd2e3ea0a3d00b4
garild <garibo@o2.pl>
2017-05-04 21:13:37 +0200

    małe zmiany

--- SkyReg/SkyReg/FrmMain.cs
+++ SkyReg/SkyReg/FrmMain.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace SkyReg
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         { 
             InitializeComponent();
         }
