021debbeaeb2fbd0a85cc318af7ba0c7a65b45a5
garild <garibo@o2.pl>
2017-05-04 21:41:48 +0200

    dodano taby

--- SkyReg/SkyReg/FrmMain.Designer.cs
+++ SkyReg/SkyReg/FrmMain.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.kryptonTabControl1 = new AC.ExtendedRenderer.Navigator.KryptonTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.kryptonTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 446);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip1.Size = new System.Drawing.Size(1101, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // kryptonTabControl1
+            // 
+            this.kryptonTabControl1.AllowCloseButton = false;
+            this.kryptonTabControl1.AllowContextButton = true;
+            this.kryptonTabControl1.AllowNavigatorButtons = false;
+            this.kryptonTabControl1.AllowSelectedTabHigh = false;
+            this.kryptonTabControl1.BorderWidth = 1;
+            this.kryptonTabControl1.Controls.Add(this.tabPage1);
+            this.kryptonTabControl1.Controls.Add(this.tabPage2);
+            this.kryptonTabControl1.CornerRoundRadiusWidth = 12;
+            this.kryptonTabControl1.CornerSymmetry = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornSymmetry.Both;
+            this.kryptonTabControl1.CornerType = AC.ExtendedRenderer.Toolkit.Drawing.DrawingMethods.CornerType.Rounded;
+            this.kryptonTabControl1.CornerWidth = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornWidth.Thin;
+            this.kryptonTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonTabControl1.HotTrack = true;
+            this.kryptonTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonTabControl1.Name = "kryptonTabControl1";
+            this.kryptonTabControl1.PreserveTabColor = false;
+            this.kryptonTabControl1.SelectedIndex = 0;
+            this.kryptonTabControl1.Size = new System.Drawing.Size(1101, 446);
+            this.kryptonTabControl1.TabIndex = 2;
+            this.kryptonTabControl1.UseExtendedLayout = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(1093, 417);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Tag = false;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Tag = false;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 468);
+            this.Controls.Add(this.kryptonTabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.kryptonTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private AC.ExtendedRenderer.Navigator.KryptonTabControl kryptonTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
