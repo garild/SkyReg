90b6ee48c6ca4c88c01c15a7e3f86543cb92d0ed
garild <garibo@o2.pl>
2017-05-04 23:29:19 +0200

    Projekt bazy ver 1

---
+++ SkyReg/SkyReg/Enums/FlightsStatuses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyReg.Enums
+{
+    public enum FlightsStatuses
+    {
+        open = 0,
+        full = 1,
+        executed = 2,
+        canceled = 3
+    }
+}
---
+++ SkyReg/SkyReg/Enums/UsersSpecialTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyReg
+{
+    public enum UserSpecialTypes
+    {
+        admin = 0,
+        systemUser = 1,
+        tandemRegistrator = 2,
+        jumper = 3,
+        passenger = 4
+    }
+}
