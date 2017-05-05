4df304da420e396a65222dee541dab6ae9f6c864
garild <garibo@o2.pl>
2017-05-05 21:45:20 +0200

    wygenerowano baze, aktualizacja zakladek

---
+++ SkyReg/DataLayer/DLModel.edmx.sql
@@ -0,0 +1,281 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/05/2017 20:44:47
+-- Generated from EDMX file: F:\Projekty 2017\Repozytorium\SkyReg\DataLayer\DLModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [db_skyreg];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'User'
+CREATE TABLE [dbo].[User] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [UsersNr] int  NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [SurName] nvarchar(max)  NOT NULL,
+    [Login] nvarchar(max)  NULL,
+    [Password] nvarchar(max)  NULL,
+    [CertificateNr] nvarchar(max)  NULL,
+    [CertificateDate] datetime  NULL,
+    [Street] nvarchar(max)  NULL,
+    [ZipCode] nvarchar(max)  NULL,
+    [City] nvarchar(max)  NULL,
+    [Phone] nvarchar(max)  NULL,
+    [Email] nvarchar(max)  NULL,
+    [Facebook] nvarchar(max)  NULL,
+    [FlightElement_Id] int  NOT NULL,
+    [TandemOrderPilot_Id] int  NOT NULL,
+    [TandemOrderPassenger_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'Role'
+CREATE TABLE [dbo].[Role] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Value] float  NULL,
+    [Camera] bit  NOT NULL,
+    [TandemPilot] bit  NOT NULL,
+    [TandemPassenger] bit  NOT NULL,
+    [SpecialType] int  NOT NULL,
+    [User_Id] int  NOT NULL,
+    [FlightElement_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'Setting'
+CREATE TABLE [dbo].[Setting] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [AssemblyValue] float  NOT NULL,
+    [CameraValue] float  NOT NULL
+);
+GO
+
+-- Creating table 'Airplane'
+CREATE TABLE [dbo].[Airplane] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Number] nvarchar(max)  NOT NULL,
+    [Seats] int  NOT NULL,
+    [Flight_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'Flight'
+CREATE TABLE [dbo].[Flight] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Number] nvarchar(max)  NOT NULL,
+    [DateTime] datetime  NOT NULL,
+    [Status] int  NOT NULL,
+    [Altitude] float  NULL
+);
+GO
+
+-- Creating table 'FlightElement'
+CREATE TABLE [dbo].[FlightElement] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Flight_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'TandemOrder'
+CREATE TABLE [dbo].[TandemOrder] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [SuggestDateTime] datetime  NOT NULL,
+    [FlightElement_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [PK_User]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Role'
+ALTER TABLE [dbo].[Role]
+ADD CONSTRAINT [PK_Role]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Setting'
+ALTER TABLE [dbo].[Setting]
+ADD CONSTRAINT [PK_Setting]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Airplane'
+ALTER TABLE [dbo].[Airplane]
+ADD CONSTRAINT [PK_Airplane]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Flight'
+ALTER TABLE [dbo].[Flight]
+ADD CONSTRAINT [PK_Flight]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'FlightElement'
+ALTER TABLE [dbo].[FlightElement]
+ADD CONSTRAINT [PK_FlightElement]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TandemOrder'
+ALTER TABLE [dbo].[TandemOrder]
+ADD CONSTRAINT [PK_TandemOrder]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [User_Id] in table 'Role'
+ALTER TABLE [dbo].[Role]
+ADD CONSTRAINT [FK_UsersRoles]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[User]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UsersRoles'
+CREATE INDEX [IX_FK_UsersRoles]
+ON [dbo].[Role]
+    ([User_Id]);
+GO
+
+-- Creating foreign key on [Flight_Id] in table 'Airplane'
+ALTER TABLE [dbo].[Airplane]
+ADD CONSTRAINT [FK_AirplaneFlight]
+    FOREIGN KEY ([Flight_Id])
+    REFERENCES [dbo].[Flight]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_AirplaneFlight'
+CREATE INDEX [IX_FK_AirplaneFlight]
+ON [dbo].[Airplane]
+    ([Flight_Id]);
+GO
+
+-- Creating foreign key on [Flight_Id] in table 'FlightElement'
+ALTER TABLE [dbo].[FlightElement]
+ADD CONSTRAINT [FK_FlightFlightElement]
+    FOREIGN KEY ([Flight_Id])
+    REFERENCES [dbo].[Flight]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_FlightFlightElement'
+CREATE INDEX [IX_FK_FlightFlightElement]
+ON [dbo].[FlightElement]
+    ([Flight_Id]);
+GO
+
+-- Creating foreign key on [FlightElement_Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [FK_UserFlightElement]
+    FOREIGN KEY ([FlightElement_Id])
+    REFERENCES [dbo].[FlightElement]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserFlightElement'
+CREATE INDEX [IX_FK_UserFlightElement]
+ON [dbo].[User]
+    ([FlightElement_Id]);
+GO
+
+-- Creating foreign key on [FlightElement_Id] in table 'Role'
+ALTER TABLE [dbo].[Role]
+ADD CONSTRAINT [FK_RoleFlightElement]
+    FOREIGN KEY ([FlightElement_Id])
+    REFERENCES [dbo].[FlightElement]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_RoleFlightElement'
+CREATE INDEX [IX_FK_RoleFlightElement]
+ON [dbo].[Role]
+    ([FlightElement_Id]);
+GO
+
+-- Creating foreign key on [TandemOrderPilot_Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [FK_TandemOrderUser]
+    FOREIGN KEY ([TandemOrderPilot_Id])
+    REFERENCES [dbo].[TandemOrder]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_TandemOrderUser'
+CREATE INDEX [IX_FK_TandemOrderUser]
+ON [dbo].[User]
+    ([TandemOrderPilot_Id]);
+GO
+
+-- Creating foreign key on [FlightElement_Id] in table 'TandemOrder'
+ALTER TABLE [dbo].[TandemOrder]
+ADD CONSTRAINT [FK_FlightElementTandemOrder]
+    FOREIGN KEY ([FlightElement_Id])
+    REFERENCES [dbo].[FlightElement]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_FlightElementTandemOrder'
+CREATE INDEX [IX_FK_FlightElementTandemOrder]
+ON [dbo].[TandemOrder]
+    ([FlightElement_Id]);
+GO
+
+-- Creating foreign key on [TandemOrderPassenger_Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [FK_UserTandemOrder]
+    FOREIGN KEY ([TandemOrderPassenger_Id])
+    REFERENCES [dbo].[TandemOrder]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserTandemOrder'
+CREATE INDEX [IX_FK_UserTandemOrder]
+ON [dbo].[User]
+    ([TandemOrderPassenger_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
--- SkyReg/SkyReg/FrmMain.Designer.cs
+++ SkyReg/SkyReg/FrmMain.Designer.cs
@@ -31,19 +31,28 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.TabsIconList = new System.Windows.Forms.ImageList(this.components);
             this.TabControl = new AC.ExtendedRenderer.Navigator.KryptonTabControl();
             this.tabPageFlights = new System.Windows.Forms.TabPage();
+            this.kryptonPanelEx1 = new AC.ExtendedRenderer.Toolkit.KryptonPanelEx();
             this.tabPageUsers = new System.Windows.Forms.TabPage();
-            this.TabsIconList = new System.Windows.Forms.ImageList(this.components);
+            this.tabPageUsersRole = new System.Windows.Forms.TabPage();
+            this.tabPageAirplanes = new System.Windows.Forms.TabPage();
             this.tabPageTandems = new System.Windows.Forms.TabPage();
             this.tabPageSettings = new System.Windows.Forms.TabPage();
-            this.tabPageAirplanes = new System.Windows.Forms.TabPage();
+            this.kryptonManager1 = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.kryptonPanelEx2 = new AC.ExtendedRenderer.Toolkit.KryptonPanelEx();
+            this.statusStrip1.SuspendLayout();
             this.TabControl.SuspendLayout();
+            this.tabPageFlights.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
             this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 446);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
@@ -51,57 +60,10 @@
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // TabControl
+            // toolStripProgressBar1
             // 
-            this.TabControl.AllowCloseButton = false;
-            this.TabControl.AllowContextButton = true;
-            this.TabControl.AllowNavigatorButtons = false;
-            this.TabControl.AllowSelectedTabHigh = false;
-            this.TabControl.BorderWidth = 1;
-            this.TabControl.Controls.Add(this.tabPageFlights);
-            this.TabControl.Controls.Add(this.tabPageUsers);
-            this.TabControl.Controls.Add(this.tabPageAirplanes);
-            this.TabControl.Controls.Add(this.tabPageTandems);
-            this.TabControl.Controls.Add(this.tabPageSettings);
-            this.TabControl.CornerRoundRadiusWidth = 12;
-            this.TabControl.CornerSymmetry = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornSymmetry.Both;
-            this.TabControl.CornerType = AC.ExtendedRenderer.Toolkit.Drawing.DrawingMethods.CornerType.Rounded;
-            this.TabControl.CornerWidth = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornWidth.Thin;
-            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TabControl.HotTrack = true;
-            this.TabControl.ImageList = this.TabsIconList;
-            this.TabControl.ItemSize = new System.Drawing.Size(80, 30);
-            this.TabControl.Location = new System.Drawing.Point(0, 0);
-            this.TabControl.Name = "TabControl";
-            this.TabControl.PreserveTabColor = false;
-            this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(1101, 446);
-            this.TabControl.TabIndex = 2;
-            this.TabControl.UseExtendedLayout = false;
-            // 
-            // tabPageFlights
-            // 
-            this.tabPageFlights.ImageIndex = 0;
-            this.tabPageFlights.Location = new System.Drawing.Point(4, 34);
-            this.tabPageFlights.Margin = new System.Windows.Forms.Padding(0);
-            this.tabPageFlights.Name = "tabPageFlights";
-            this.tabPageFlights.Size = new System.Drawing.Size(1093, 408);
-            this.tabPageFlights.TabIndex = 0;
-            this.tabPageFlights.Tag = false;
-            this.tabPageFlights.Text = "Wyloty";
-            this.tabPageFlights.UseVisualStyleBackColor = true;
-            // 
-            // tabPageUsers
-            // 
-            this.tabPageUsers.ImageIndex = 4;
-            this.tabPageUsers.Location = new System.Drawing.Point(4, 25);
-            this.tabPageUsers.Margin = new System.Windows.Forms.Padding(0);
-            this.tabPageUsers.Name = "tabPageUsers";
-            this.tabPageUsers.Size = new System.Drawing.Size(1093, 417);
-            this.tabPageUsers.TabIndex = 1;
-            this.tabPageUsers.Tag = false;
-            this.tabPageUsers.Text = "Skoczkowie";
-            this.tabPageUsers.UseVisualStyleBackColor = true;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 16);
             // 
             // TabsIconList
             // 
@@ -113,41 +75,141 @@
             this.TabsIconList.Images.SetKeyName(3, "parachute.png");
             this.TabsIconList.Images.SetKeyName(4, "user.png");
             // 
-            // tabPageTandems
+            // TabControl
             // 
-            this.tabPageTandems.ImageIndex = 2;
-            this.tabPageTandems.Location = new System.Drawing.Point(4, 25);
-            this.tabPageTandems.Margin = new System.Windows.Forms.Padding(0);
-            this.tabPageTandems.Name = "tabPageTandems";
-            this.tabPageTandems.Size = new System.Drawing.Size(1093, 417);
-            this.tabPageTandems.TabIndex = 2;
-            this.tabPageTandems.Tag = false;
-            this.tabPageTandems.Text = "Tandemy";
-            this.tabPageTandems.UseVisualStyleBackColor = true;
+            this.TabControl.AllowCloseButton = false;
+            this.TabControl.AllowContextButton = false;
+            this.TabControl.AllowNavigatorButtons = false;
+            this.TabControl.AllowSelectedTabHigh = false;
+            this.TabControl.BorderWidth = 1;
+            this.TabControl.Controls.Add(this.tabPageFlights);
+            this.TabControl.Controls.Add(this.tabPageUsers);
+            this.TabControl.Controls.Add(this.tabPageUsersRole);
+            this.TabControl.Controls.Add(this.tabPageAirplanes);
+            this.TabControl.Controls.Add(this.tabPageTandems);
+            this.TabControl.Controls.Add(this.tabPageSettings);
+            this.TabControl.CornerRoundRadiusWidth = 2;
+            this.TabControl.CornerSymmetry = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornSymmetry.Both;
+            this.TabControl.CornerType = AC.ExtendedRenderer.Toolkit.Drawing.DrawingMethods.CornerType.Rounded;
+            this.TabControl.CornerWidth = AC.ExtendedRenderer.Navigator.KryptonTabControl.CornWidth.Null;
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.TabControl.HotTrack = true;
+            this.TabControl.ImageList = this.TabsIconList;
+            this.TabControl.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.TabControl.ItemSize = new System.Drawing.Size(80, 30);
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.PreserveTabColor = false;
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(1101, 446);
+            this.TabControl.TabIndex = 3;
+            this.TabControl.UseExtendedLayout = false;
             // 
-            // tabPageSettings
+            // tabPageFlights
             // 
-            this.tabPageSettings.ImageIndex = 1;
-            this.tabPageSettings.Location = new System.Drawing.Point(4, 25);
-            this.tabPageSettings.Margin = new System.Windows.Forms.Padding(0);
-            this.tabPageSettings.Name = "tabPageSettings";
-            this.tabPageSettings.Size = new System.Drawing.Size(1093, 417);
-            this.tabPageSettings.TabIndex = 3;
-            this.tabPageSettings.Tag = false;
-            this.tabPageSettings.Text = "Ustawienia";
-            this.tabPageSettings.UseVisualStyleBackColor = true;
+            this.tabPageFlights.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageFlights.Controls.Add(this.kryptonPanelEx2);
+            this.tabPageFlights.Controls.Add(this.kryptonPanelEx1);
+            this.tabPageFlights.ImageIndex = 0;
+            this.tabPageFlights.Location = new System.Drawing.Point(4, 34);
+            this.tabPageFlights.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageFlights.Name = "tabPageFlights";
+            this.tabPageFlights.Size = new System.Drawing.Size(1093, 408);
+            this.tabPageFlights.TabIndex = 0;
+            this.tabPageFlights.Tag = false;
+            this.tabPageFlights.Text = "Wyloty";
+            // 
+            // kryptonPanelEx1
+            // 
+            this.kryptonPanelEx1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonPanelEx1.GradientDirection = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.kryptonPanelEx1.GradientToogleColors = false;
+            this.kryptonPanelEx1.GradientUseBlend = false;
+            this.kryptonPanelEx1.Image = null;
+            this.kryptonPanelEx1.ImageLocation = new System.Drawing.Point(4, 4);
+            this.kryptonPanelEx1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanelEx1.Name = "kryptonPanelEx1";
+            this.kryptonPanelEx1.PersistentColors = false;
+            this.kryptonPanelEx1.Size = new System.Drawing.Size(1093, 30);
+            this.kryptonPanelEx1.TabIndex = 0;
+            // 
+            // tabPageUsers
+            // 
+            this.tabPageUsers.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageUsers.ImageIndex = 4;
+            this.tabPageUsers.Location = new System.Drawing.Point(4, 34);
+            this.tabPageUsers.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageUsers.Name = "tabPageUsers";
+            this.tabPageUsers.Size = new System.Drawing.Size(1093, 408);
+            this.tabPageUsers.TabIndex = 1;
+            this.tabPageUsers.Tag = false;
+            this.tabPageUsers.Text = "Skoczkowie";
+            // 
+            // tabPageUsersRole
+            // 
+            this.tabPageUsersRole.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageUsersRole.Location = new System.Drawing.Point(4, 34);
+            this.tabPageUsersRole.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageUsersRole.Name = "tabPageUsersRole";
+            this.tabPageUsersRole.Size = new System.Drawing.Size(1093, 408);
+            this.tabPageUsersRole.TabIndex = 5;
+            this.tabPageUsersRole.Tag = false;
+            this.tabPageUsersRole.Text = "Typy skoczków";
             // 
             // tabPageAirplanes
             // 
+            this.tabPageAirplanes.BackColor = System.Drawing.SystemColors.Window;
             this.tabPageAirplanes.ImageIndex = 0;
-            this.tabPageAirplanes.Location = new System.Drawing.Point(4, 25);
+            this.tabPageAirplanes.Location = new System.Drawing.Point(4, 34);
             this.tabPageAirplanes.Margin = new System.Windows.Forms.Padding(0);
             this.tabPageAirplanes.Name = "tabPageAirplanes";
-            this.tabPageAirplanes.Size = new System.Drawing.Size(1093, 417);
+            this.tabPageAirplanes.Size = new System.Drawing.Size(1093, 408);
             this.tabPageAirplanes.TabIndex = 4;
             this.tabPageAirplanes.Tag = false;
             this.tabPageAirplanes.Text = "Samoloty";
-            this.tabPageAirplanes.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTandems
+            // 
+            this.tabPageTandems.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageTandems.ImageIndex = 2;
+            this.tabPageTandems.Location = new System.Drawing.Point(4, 34);
+            this.tabPageTandems.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageTandems.Name = "tabPageTandems";
+            this.tabPageTandems.Size = new System.Drawing.Size(1093, 408);
+            this.tabPageTandems.TabIndex = 2;
+            this.tabPageTandems.Tag = false;
+            this.tabPageTandems.Text = "Tandemy";
+            // 
+            // tabPageSettings
+            // 
+            this.tabPageSettings.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPageSettings.ImageIndex = 1;
+            this.tabPageSettings.Location = new System.Drawing.Point(4, 34);
+            this.tabPageSettings.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageSettings.Name = "tabPageSettings";
+            this.tabPageSettings.Size = new System.Drawing.Size(1093, 408);
+            this.tabPageSettings.TabIndex = 3;
+            this.tabPageSettings.Tag = false;
+            this.tabPageSettings.Text = "Ustawienia";
+            // 
+            // kryptonManager1
+            // 
+            this.kryptonManager1.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.Office2007Blue;
+            // 
+            // kryptonPanelEx2
+            // 
+            this.kryptonPanelEx2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanelEx2.GradientDirection = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.kryptonPanelEx2.GradientToogleColors = false;
+            this.kryptonPanelEx2.GradientUseBlend = false;
+            this.kryptonPanelEx2.Image = null;
+            this.kryptonPanelEx2.ImageLocation = new System.Drawing.Point(4, 4);
+            this.kryptonPanelEx2.Location = new System.Drawing.Point(0, 378);
+            this.kryptonPanelEx2.Name = "kryptonPanelEx2";
+            this.kryptonPanelEx2.PersistentColors = false;
+            this.kryptonPanelEx2.Size = new System.Drawing.Size(1093, 30);
+            this.kryptonPanelEx2.TabIndex = 2;
             // 
             // FrmMain
             // 
@@ -159,7 +221,10 @@
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SkyReg";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.TabControl.ResumeLayout(false);
+            this.tabPageFlights.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,13 +233,18 @@
         #endregion
 
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ImageList TabsIconList;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
         private AC.ExtendedRenderer.Navigator.KryptonTabControl TabControl;
         private System.Windows.Forms.TabPage tabPageFlights;
+        private AC.ExtendedRenderer.Toolkit.KryptonPanelEx kryptonPanelEx1;
         private System.Windows.Forms.TabPage tabPageUsers;
-        private System.Windows.Forms.ImageList TabsIconList;
+        private System.Windows.Forms.TabPage tabPageUsersRole;
+        private System.Windows.Forms.TabPage tabPageAirplanes;
         private System.Windows.Forms.TabPage tabPageTandems;
         private System.Windows.Forms.TabPage tabPageSettings;
-        private System.Windows.Forms.TabPage tabPageAirplanes;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager1;
+        private AC.ExtendedRenderer.Toolkit.KryptonPanelEx kryptonPanelEx2;
     }
 }
 
